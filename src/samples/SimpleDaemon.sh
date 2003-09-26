849d7297e23070fac3a1739d53e2aa5b36e504d7
Jean-Frederic Clere <jfclere@apache.org>
2003-09-26 21:15:24 +0000

    Arrange the example to get it running more easly.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/daemon/trunk@140682 13f79535-47bb-0310-9956-ffa450edef68

--- src/samples/SimpleDaemon.sh
+++ src/samples/SimpleDaemon.sh
@@ -3,18 +3,19 @@
 # Small shell script to show how to start the sample services.
 #
 # Adapt the following lines to your configuration
-JAVA_HOME=/usr/java/jdk1.3.1
-DAEMON_HOME=/home1/jakarta/jakarta-commons-sandbox/daemon
-TOMCAT_USER=jakarta
+JAVA_HOME=`echo $JAVA_HOME`
+DAEMON_HOME=`(cd ../..; pwd)`
+TOMCAT_USER=`echo $USER`
 CLASSPATH=\
 $DAEMON_HOME/dist/commons-daemon.jar:\
 $DAEMON_HOME/dist/SimpleDaemon.jar
 
-$DAEMON_HOME/dist/jsvc \
+$DAEMON_HOME/src/native/unix/jsvc \
     -user $TOMCAT_USER \
     -home $JAVA_HOME \
     -cp $CLASSPATH \
-    SimpleDaemon 
+    -pidfile ./pidfile \
+    SimpleDaemon \
 #
 # To get a verbose JVM
 #-verbose \
