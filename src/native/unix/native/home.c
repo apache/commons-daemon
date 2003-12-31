59eb037db0219292dfc443d0d2d0e6c339027aa4
William Barker <billbarker@apache.org>
2003-12-31 04:52:30 +0000

    Fix parsing of 'jvm.cfg' files for the new 1.4.x format.
    Fix for Bug #25790
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/daemon/trunk@140724 13f79535-47bb-0310-9956-ffa450edef68

--- src/native/unix/native/home.c
+++ src/native/unix/native/home.c
@@ -55,7 +55,7 @@
  *                                                                           *
  * ========================================================================= */
 
-/* @version $Id: home.c,v 1.1 2003/09/04 23:28:20 yoavs Exp $ */
+/* @version $Id: home.c,v 1.2 2003/12/31 04:52:30 billbarker Exp $ */
 #include "jsvc.h"
 
 /* Check if a path is a directory */
@@ -81,7 +81,7 @@ static bool checkfile(char *path) {
 /* Parse a VM configuration file */
 static bool parse(home_data *data) {
     FILE *cfgf=fopen(data->cfgf,"r");
-    char *ret=NULL;
+    char *ret=NULL, *sp;
     char buf[1024];
 
     if (cfgf==NULL) return(false);
@@ -104,6 +104,10 @@ static bool parse(home_data *data) {
                 ret[pos--]='\0';
             } else break;
         }
+        /* Format changed for 1.4 JVMs */
+        sp = strchr(ret, ' ');
+        if(sp != NULL)
+            *sp = '\0'
 
         /* Did we find something significant? */
         if (strlen(ret)>0) {
