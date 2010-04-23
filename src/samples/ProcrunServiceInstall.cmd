d640d800637bbc32f5a44b33fd32062af6dd2dcd
Sebastian Bazley <sebb@apache.org>
2010-04-23 14:39:11 +0000

    Comments
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/daemon/trunk@937314 13f79535-47bb-0310-9956-ffa450edef68

--- src/samples/ProcrunServiceInstall.cmd
+++ src/samples/ProcrunServiceInstall.cmd
@@ -15,7 +15,7 @@ rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem See the License for the specific language governing permissions and
 rem limitations under the License.
 
-rem Batch script for defining the ProcrunService
+rem Batch script for defining the ProcrunService (JVM and Java versions)
 
 rem Copy this file and ProcrunService.jar into the same directory as prunsrv (or adjust the paths below)
 
