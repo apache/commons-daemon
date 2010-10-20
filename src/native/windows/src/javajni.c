9232e81f14aa744a4700583b4970db77e8ad9b03
Mladen Turk <mturk@apache.org>
2010-10-20 05:28:27 +0000

    DAEMON-163 Code cleanup.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/daemon/trunk@1024527 13f79535-47bb-0310-9956-ffa450edef68

--- src/native/windows/src/javajni.c
+++ src/native/windows/src/javajni.c
@@ -392,10 +392,7 @@ static DWORD __apxMultiSzToJvmOptions(APXHANDLE hPool,
         l = __apxGetMultiSzLengthA(lpString, &n);
     }
     n += nExtra;
-    if (IS_INVALID_HANDLE(hPool))
-        buff = apxPoolAlloc(hPool, (n + 1) * sizeof(JavaVMOption) + (l + 1));
-    else
-        buff = apxAlloc((n + 1) * sizeof(JavaVMOption) + (l + 1));
+    buff = apxPoolAlloc(hPool, (n + 1) * sizeof(JavaVMOption) + (l + 1));
 
     *lppArray = (JavaVMOption *)buff;
     p = (LPSTR)(buff + (n + 1) * sizeof(JavaVMOption));
@@ -914,7 +911,7 @@ apxJavaStart(LPAPXJAVA_THREADARGS pArgs)
     if (lstrcmpA(lpJava->clWorker.sClazz, "java/lang/System")) {
         /* Give some time to initialize the thread
          * Unless we are calling System.exit(0).
-         * This will be hanled by _onexit hook. 
+         * This will be hanled by _onexit hook.
          */
         Sleep(1000);
     }
