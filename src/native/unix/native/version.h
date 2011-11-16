6a7f5ef4d0d4a26bcd8f2e4a64ea135efd947b1b
Mladen Turk <mturk@apache.org>
2011-11-16 12:17:56 +0000

    Bump version to 1.0.8
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/daemon/trunk@1202656 13f79535-47bb-0310-9956-ffa450edef68

--- src/native/unix/native/version.h
+++ src/native/unix/native/version.h
@@ -31,7 +31,7 @@
 #define JSVC_MINOR_VERSION      0
 
 /** patch level */
-#define JSVC_PATCH_VERSION      7
+#define JSVC_PATCH_VERSION      8
 
 /**
  *  This symbol is defined for internal, "development" copies of JSVC.
