cc5088a2eb1ed93fdfdeb4f089f3396a64a4f1c4
Mladen Turk <mturk@apache.org>
2013-10-18 07:40:05 +0000

    Apply fix for DAEMON-281
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/daemon/branches/1.0.x@1533348 13f79535-47bb-0310-9956-ffa450edef68

--- src/native/unix/native/location.c
+++ src/native/unix/native/location.c
@@ -143,6 +143,7 @@ char *location_jvm_default[] = {
  */
 char *location_jvm_configured[] = {
 #if defined(OS_DARWIN)
+    "$JAVA_HOME/jre/lib/$VM_NAME/libjvm.dylib",
     "$JAVA_HOME/../Libraries/lib$VM_NAME.dylib",
 #elif defined(OS_CYGWIN)
     "$JAVA_HOME/jre/bin/$VM_NAME/jvm.dll",              /* Sun JDK 1.3 */
