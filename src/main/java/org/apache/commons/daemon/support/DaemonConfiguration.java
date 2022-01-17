8892198668e75be0ff9f43dcc79d526baed94120
Mark Thomas <markt@apache.org>
2022-01-17 10:38:37 +0000

    Remove out-dated comment

--- src/main/java/org/apache/commons/daemon/support/DaemonConfiguration.java
+++ src/main/java/org/apache/commons/daemon/support/DaemonConfiguration.java
@@ -139,7 +139,7 @@ public final class DaemonConfiguration
             if (sysvalue == null) {
                 // Try with the environment if there was no
                 // property by that name.
-                sysvalue = System.getenv(variable); // N.B. Deprecated in Java 1.3/1.4, but re-instated in Java 1.5+
+                sysvalue = System.getenv(variable);
             }
             if (sysvalue != null) {
                 final String strtoken = propValue.substring(ctoken, btoken);
