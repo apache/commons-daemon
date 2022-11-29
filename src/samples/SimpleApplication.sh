073eb38c085bfb530aaeb6a2fed4726f2b0abb2f
Mark Thomas <markt@apache.org>
2022-11-29 11:48:54 +0000

    Update version to 1.3.4 for next development cycle

--- src/samples/SimpleApplication.sh
+++ src/samples/SimpleApplication.sh
@@ -20,7 +20,7 @@
 # Adapt the following lines to your configuration
 JAVA_HOME=/opt/java6
 PROGRAM=SimpleApplication
-CLASSPATH=`pwd`/$PROGRAM.jar:`pwd`/commons-daemon-1.3.3.jar
+CLASSPATH=`pwd`/$PROGRAM.jar:`pwd`/commons-daemon-1.3.4.jar
 
 case "$1" in
   start )
