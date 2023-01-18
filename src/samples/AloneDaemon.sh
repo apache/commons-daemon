ae296022eb3abed3c77e20767adb9ff5a3f30705
Gary Gregory <garydgregory@gmail.com>
2023-01-18 09:34:40 -0500

    Fix spelling

--- src/samples/AloneDaemon.sh
+++ src/samples/AloneDaemon.sh
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 # Small shell script to show how to start the sample services.
-# That is for linux, if your are using cygwin look to ServiceDaemon.sh.
+# That is for Linux, if your are using cygwin look to ServiceDaemon.sh.
 #
 # Adapt the following lines to your configuration
 JAVA_HOME=`echo $JAVA_HOME`
