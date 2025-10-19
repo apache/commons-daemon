697134177078b68ee2558072b1e9196ada5e63f5
Gary D. Gregory <garydgregory@gmail.com>
2025-10-19 09:19:03 -0400

    Use HTTPS in license link

--- src/native/windows/apps/prunsrv/test/scripts/mybanner.bat
+++ src/native/windows/apps/prunsrv/test/scripts/mybanner.bat
@@ -6,7 +6,7 @@ rem The ASF licenses this file to You under the Apache License, Version 2.0
 rem (the "License"); you may not use this file except in compliance with
 rem the License.  You may obtain a copy of the License at
 rem
-rem     http://www.apache.org/licenses/LICENSE-2.0
+rem     https://www.apache.org/licenses/LICENSE-2.0
 rem
 rem Unless required by applicable law or agreed to in writing, software
 rem distributed under the License is distributed on an "AS IS" BASIS,
