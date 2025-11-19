388d648e5794d9520f9c8b0d547d0743ebdeee44
Mark Thomas <markt@apache.org>
2025-11-19 14:50:44 +0000

    Remaining version updates for 1.5.0

--- src/native/unix/native/version.h
+++ src/native/unix/native/version.h
@@ -28,10 +28,10 @@
  * Minor API changes that do not cause binary compatibility problems.
  * Should be reset to 0 when upgrading JSVC_MAJOR_VERSION
  */
-#define JSVC_MINOR_VERSION      4
+#define JSVC_MINOR_VERSION      5
 
 /** patch level */
-#define JSVC_PATCH_VERSION      1
+#define JSVC_PATCH_VERSION      0
 
 /**
  *  This symbol is defined for internal, "development" copies of JSVC.
