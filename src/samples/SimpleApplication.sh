24f3570ed37d6a583e916be98c8285d87beda5ec
Mark Thomas <markt@apache.org>
2025-12-01 08:30:05 +0000

    Increment version number of next development cycle

--- src/samples/SimpleApplication.sh
+++ src/samples/SimpleApplication.sh
@@ -20,7 +20,7 @@
 # Adapt the following lines to your configuration
 JAVA_HOME=/opt/java6
 PROGRAM=SimpleApplication
-CLASSPATH=`pwd`/$PROGRAM.jar:`pwd`/commons-daemon-1.5.0.jar
+CLASSPATH=`pwd`/$PROGRAM.jar:`pwd`/commons-daemon-1.5.1.jar
 
 case "$1" in
   start )
